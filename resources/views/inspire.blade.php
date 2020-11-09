641b6432b8cdcae32efd9b9f23e42fa0206c870f
huzy01 <huzy01@mingyuanyun.com>
2020-11-09 11:29:09 +0800

    fix page tile of inspire view

--- resources/views/inspire.blade.php
+++ resources/views/inspire.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Aliyun Sae Laravel Demo</title>
+        <title>Aliyun FC Laravel Demo</title>
 
         <!-- Styles -->
         <style>
